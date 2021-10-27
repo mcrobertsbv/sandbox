690f00975e52758c2ed612d0a64007fcbad86aa3
Sietze de Haan <s.dehaan@mcroberts.nl>
2021-10-27 13:20:47 +0000

    Update failed_test.R changed version number in testthat

--- failed_test.R
+++ failed_test.R
@@ -2,7 +2,7 @@ library("multiversion", character.only = T,
             lib.loc = paste(Sys.getenv('R_VC_LIBRARY_LOCATION'), 
                             'multiversion/0.2.0', sep = '/'))
 
-lib.load(testthat = '1.0.2')
+lib.load(testthat = '3.0.4')
 
 test_that("fail test", {
   expect_equal(1, 0)
