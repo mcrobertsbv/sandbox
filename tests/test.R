2e53ee72d19b4c5c5b7d5ee0c2ad3ed5b762ddd6
Laura Siepman <l.siepman@mcroberts.nl>
2021-11-29 09:56:57 +0100

    Test creating multiple xml files, one for R and one for python.

--- tests/test.R
+++ tests/test.R
@@ -2,9 +2,9 @@ library("multiversion", character.only = T,
         lib.loc = paste(Sys.getenv('R_VC_LIBRARY_LOCATION'),
                         'multiversion/0.2.0', sep = '/'))
 
-suppressWarnings(lib.load(testthat = '1.0.2', quietly = T))
-# library(testthat)
-options(testthat.output_file = "test-out.xml")
-test_dir("tests/testthat/", reporter = c("Summary", "Fail"))
+suppressWarnings(lib.load(testthat = '3.1.0', quietly = T))
+
+options(testthat.output_file = "./tests/R_test_out.xml")
+test_dir("tests/testthat/", reporter = "Junit")
 
 
---
+++ tests/test_python.py
@@ -0,0 +1,16 @@
+import os
+import unittest
+import xmlrunner
+
+class InitialTests(unittest.TestCase):
+
+    def test_failure(self):
+        """Test what happens when a unittest fails"""
+        self.assertTrue("apples" == "oranges", "You can't compare apples with oranges")
+
+    def test_succeed(self):
+        """Test what happens when a test succeeds"""
+        self.assertTrue("apples" == "apples")
+
+with open(str(os.getcwd()) + '/testresults.xml', 'w+b') as output:
+    unittest.main(testRunner = xmlrunner.XMLTestRunner(output=output))
